8ef47c2ab50b928e550f3f18b61cdda82592eac1
Piotr Korcz <ptrkorcz@gmail.com>
2026-05-18 12:27:57 +0200

    ngtcp2 project version

--- cmake/Findngtcp2.cmake
+++ cmake/Findngtcp2.cmake
@@ -36,7 +36,67 @@ set (NGTCP2_DISABLE_TESTS
 
 enable_language (C)
 
+# cmake-cooking wraps project() and skips nested invocations in subdirectories.
+# Seed ngtcp2's project version variables so its CMakeLists can still derive
+# PACKAGE_VERSION_NUM when built through Seastar's DPDK workflow.
+if (DEFINED PROJECT_VERSION)
+  set (_ngtcp2_saved_project_version "${PROJECT_VERSION}")
+  set (_ngtcp2_saved_project_version_defined ON)
+else ()
+  set (_ngtcp2_saved_project_version_defined OFF)
+endif ()
+
+if (DEFINED PROJECT_VERSION_MAJOR)
+  set (_ngtcp2_saved_project_version_major "${PROJECT_VERSION_MAJOR}")
+  set (_ngtcp2_saved_project_version_major_defined ON)
+else ()
+  set (_ngtcp2_saved_project_version_major_defined OFF)
+endif ()
+
+if (DEFINED PROJECT_VERSION_MINOR)
+  set (_ngtcp2_saved_project_version_minor "${PROJECT_VERSION_MINOR}")
+  set (_ngtcp2_saved_project_version_minor_defined ON)
+else ()
+  set (_ngtcp2_saved_project_version_minor_defined OFF)
+endif ()
+
+if (DEFINED PROJECT_VERSION_PATCH)
+  set (_ngtcp2_saved_project_version_patch "${PROJECT_VERSION_PATCH}")
+  set (_ngtcp2_saved_project_version_patch_defined ON)
+else ()
+  set (_ngtcp2_saved_project_version_patch_defined OFF)
+endif ()
+
+set (PROJECT_VERSION "1.21.90")
+set (PROJECT_VERSION_MAJOR 1)
+set (PROJECT_VERSION_MINOR 21)
+set (PROJECT_VERSION_PATCH 90)
+
 add_subdirectory (ngtcp2)
+
+if (_ngtcp2_saved_project_version_defined)
+  set (PROJECT_VERSION "${_ngtcp2_saved_project_version}")
+else ()
+  unset (PROJECT_VERSION)
+endif ()
+
+if (_ngtcp2_saved_project_version_major_defined)
+  set (PROJECT_VERSION_MAJOR "${_ngtcp2_saved_project_version_major}")
+else ()
+  unset (PROJECT_VERSION_MAJOR)
+endif ()
+
+if (_ngtcp2_saved_project_version_minor_defined)
+  set (PROJECT_VERSION_MINOR "${_ngtcp2_saved_project_version_minor}")
+else ()
+  unset (PROJECT_VERSION_MINOR)
+endif ()
+
+if (_ngtcp2_saved_project_version_patch_defined)
+  set (PROJECT_VERSION_PATCH "${_ngtcp2_saved_project_version_patch}")
+else ()
+  unset (PROJECT_VERSION_PATCH)
+endif ()
 
 set (NGTCP2_SRC "${CMAKE_CURRENT_SOURCE_DIR}/ngtcp2")
 set (NGTCP2_BIN "${CMAKE_CURRENT_BINARY_DIR}/ngtcp2")
