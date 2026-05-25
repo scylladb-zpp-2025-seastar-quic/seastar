bbfd97fac925cc6320ac652a004afc19aa7d4bfd
Pawel Pery <pawel.pery@scylladb.com>
2026-05-25 10:56:41 +0200

    fix quic precommit sources

--- cmake/Findngtcp2.cmake
+++ cmake/Findngtcp2.cmake
@@ -1,37 +1,37 @@
-set (NGTCP2_ENABLE_LIB_ONLY    
-  ON  
-  CACHE 
-  BOOL 
-  "Build libngtcp2 only")
-
-set (NGTCP2_ENABLE_STATIC_LIB  
-  ON  
-  CACHE 
-  BOOL 
-  "Build static lib")
-
-set (NGTCP2_ENABLE_SHARED_LIB  
-  OFF 
-  CACHE 
-  BOOL 
-  "Disable shared lib")
-
-set (NGTCP2_ENABLE_GNUTLS      
-  ON  
-  CACHE 
-  BOOL 
-  "Enable GnuTLS")
-
-set (NGTCP2_ENABLE_OPENSSL     
-  OFF 
-  CACHE 
-  BOOL 
-  "Disable OpenSSL")
-
-set (NGTCP2_DISABLE_TESTS      
+set (NGTCP2_ENABLE_LIB_ONLY
   ON
   CACHE
-  BOOL 
+  BOOL
+  "Build libngtcp2 only")
+
+set (NGTCP2_ENABLE_STATIC_LIB
+  ON
+  CACHE
+  BOOL
+  "Build static lib")
+
+set (NGTCP2_ENABLE_SHARED_LIB
+  OFF
+  CACHE
+  BOOL
+  "Disable shared lib")
+
+set (NGTCP2_ENABLE_GNUTLS
+  ON
+  CACHE
+  BOOL
+  "Enable GnuTLS")
+
+set (NGTCP2_ENABLE_OPENSSL
+  OFF
+  CACHE
+  BOOL
+  "Disable OpenSSL")
+
+set (NGTCP2_DISABLE_TESTS
+  ON
+  CACHE
+  BOOL
   "Disable tests")
 
 enable_language (C)
@@ -44,19 +44,19 @@ set (NGTCP2_SRC "${CMAKE_CURRENT_SOURCE_DIR}/ngtcp2")
 set (NGTCP2_BIN "${CMAKE_CURRENT_BINARY_DIR}/ngtcp2")
 
 set (ngtcp2_INCLUDE_DIRS
-  "${NGTCP2_SRC}/lib/includes"     
-  "${NGTCP2_SRC}/crypto/includes"  
-  "${NGTCP2_SRC}/lib"              
-  "${NGTCP2_BIN}/lib/includes"     
+  "${NGTCP2_SRC}/lib/includes"
+  "${NGTCP2_SRC}/crypto/includes"
+  "${NGTCP2_SRC}/lib"
+  "${NGTCP2_BIN}/lib/includes"
 )
 
 if (TARGET ngtcp2_static AND TARGET ngtcp2_crypto_gnutls_static)
   add_library (ngtcp2::ngtcp2 INTERFACE IMPORTED)
 
-  set_target_properties(ngtcp2_static PROPERTIES 
+  set_target_properties(ngtcp2_static PROPERTIES
     POSITION_INDEPENDENT_CODE ON
   )
-  
+
   set_target_properties(ngtcp2_crypto_gnutls_static PROPERTIES
     POSITION_INDEPENDENT_CODE ON
   )
@@ -78,6 +78,6 @@ endif()
 
 include (FindPackageHandleStandardArgs)
 
-find_package_handle_standard_args (ngtcp2 
-  REQUIRED_VARS 
+find_package_handle_standard_args (ngtcp2
+  REQUIRED_VARS
     ngtcp2_FOUND)
